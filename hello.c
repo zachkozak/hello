b0b3c892d41f1607fef316a313148db88724f2be
Zach Kozak <zkozak2@huskers.unl.edu>
2022-08-25 16:29:43 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Zach Kozak
+ *         zkozak2@huskers.unl.edu
+ * Date: 2022/08/25
+ * For hack 1: C edition
+ * A simple hello world program in C, committing to GIT!
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  printf("Hello World!\n");
+  printf("Zach Kozak!\n");
+  printf("Computer Science major, math minor whether I want to or not :D\n");
+
+  return 0;
+}
